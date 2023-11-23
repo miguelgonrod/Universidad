917242f99cb82fef755e970ac47b0618f115f88b
miguelgonrod <miguelgonrod2004@gmail.com>
2023-11-22 23:33:30 -0500

    added TC taller 2

---
+++ teoriaComputacion/taller2/programa.js
@@ -0,0 +1,20 @@
+// Problema 1 (20%). Implemente la siguiente función
+function buscarPorZonaHoraria(paises, abreviacionZonaHoraria) {
+  /*
+          Retorna una lista con los nombres de todos los países que tengan la zona horaria
+          indicada en el 2o parámetro
+  
+      */
+}
+
+// Problema 2 (30%). Implemente la siguiente función
+function buscarTraducciones(paises, nombre) {
+  /*
+          Retorna una lista con todas las traducciones de un país cuyo nombre
+          se pasa como parámetro
+  
+      */
+}
+
+console.log(buscarPorZonaHoraria(paises, "CET")); // Debe retornar ["Albania", "Algeria", "Andorra", ... ]
+console.log(buscarTraducciones(paises, "Andorra")); // Debe retornar ["안도라", "Andorra", "Andorra", "Andorra", "Andora", "آندورا", "Andorra", "Andorra", "Andorre", "アンドラ", "Andorra", "安道尔"]
